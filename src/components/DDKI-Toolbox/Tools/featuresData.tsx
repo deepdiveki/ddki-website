81044cc65934bb6e5c2abe0f35caebc0a12610a3
deepdiveki <info@deepdive-ki.de>
2025-02-22 13:22:19 +0100

    link ki chat

--- src/components/DDKI-Toolbox/Tools/featuresData.tsx
+++ src/components/DDKI-Toolbox/Tools/featuresData.tsx
@@ -34,7 +34,7 @@ const ddkitools: Feature[] = [
     description: "Nutze die besten LLM'S für deine Chats. GPT 4o, Llama 3.1, Claude 3 Opus",
     icon: "/images/features/icon-02.svg",
     animationComponent: chatbotAnimation,
-    link: "https://toolbox.deepdive-ki.de/ki-chat",
+    link: "https://toolbox.deepdive-ki.de/",
   },
   {
     id: 21,
