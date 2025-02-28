fe22336c1e14a6febfcef71b7db8200b9ab58cf0
Tim Philipp <info@deepdive-ki.de>
2025-02-28 01:17:35 +0100

    access??

--- src/app/(site)/admin/page.tsx
+++ src/app/(site)/admin/page.tsx
@@ -3,11 +3,11 @@
 import { useSession } from "next-auth/react";
 
 export default function AdminPage() {
-  const { data: session } = useSession();
-
-  if (!session || session.user.access !== "admin") {
-    return <p>Access denied</p>;
-  }
+//   const { data: session } = useSession();
+//
+//   if (!session || session.user.access !== "admin") {
+//     return <p>Access denied</p>;
+//   }
 
   return <h1>Welcome, Admin</h1>;
 }
