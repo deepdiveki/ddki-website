bcb030fb6eee1d0ac9017ebcd933480f25937a78
Tim Philipp <info@deepdive-ki.de>
2025-03-08 21:20:31 +0100

    pos fix7 - persistant user session change

--- src/app/api/dbAccessCheck/route.ts
+++ src/app/api/dbAccessCheck/route.ts
@@ -1,7 +1,9 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/libs/auth";
+import { getToken } from "next-auth/jwt";
+import { AdapterUser } from "next-auth/adapters";
 
 const prisma = new PrismaClient();
 
@@ -15,7 +17,7 @@ const corsHeaders = {
 /**
  * GET request to check if a user's session access matches the database.
  */
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   try {
 
     const { searchParams } = new URL(request.url);
@@ -53,7 +55,7 @@ export async function GET(request: Request) {
   }
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
 
   const session = await getServerSession(authOptions);
 
@@ -65,6 +67,26 @@ export async function POST(request: Request) {
 
   // Update session data
   session.user.access = newAccess;
+
+  if (!authOptions.callbacks?.session) {
+    throw new Error("Session callback is not defined in authOptions");
+  }
+
+  const token = await getToken({ req: request });
+
+    const jwtToken = token || {
+    user: session.user, // Include the required `user` property
+    // Add other required JWT properties if needed
+  };
+
+  // Save the updated session to the session store
+  await authOptions.callbacks.session({
+    session,
+    token: jwtToken,
+    user: session.user as unknown as AdapterUser,
+    newSession: false, // Indicate whether this is a new session
+    trigger: "update", // Indicate the trigger for the session update
+  });
 
   console.log('Haloooooooooo?');
 
