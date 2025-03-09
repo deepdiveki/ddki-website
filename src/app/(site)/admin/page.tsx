ed43957ca89ba3efafbc5e9334db5975ab8298b6
Tim Philipp <info@deepdive-ki.de>
2025-03-09 11:24:05 +0100

    admin site debug

--- src/app/(site)/admin/page.tsx
+++ src/app/(site)/admin/page.tsx
@@ -8,6 +8,12 @@ import AdminSeite from "./AdminSeite";
 export default function AdminPage() {
   const { data: session } = useSession();
 
+  if (status === "loading") {
+    return <p>Loading...</p>; // Show a loading spinner or message
+  }
+
+  console.log("admin page access?: ", session?.user?.access);
+
   if (!session || session.user.access !== "admin") {
     redirect("/");
   }
