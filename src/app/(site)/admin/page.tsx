c681173511d2a637bb4f8f5ae3f618b8f603cbcc
Tim Philipp <info@deepdive-ki.de>
2025-03-09 11:28:59 +0100

    typo fix

--- src/app/(site)/admin/page.tsx
+++ src/app/(site)/admin/page.tsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 import AdminSeite from "./AdminSeite";
 
 export default function AdminPage() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
 
   if (status === "loading") {
     return <p>Loading...</p>; // Show a loading spinner or message
